3b4dc5fc661e40e118c5dd2fcb0364bcd356969a
Carlos Cuevas <ccuevasbalinas@gmail.com>
2021-11-22 18:42:31 +0100

    Update KNN

--- KNN.m
+++ KNN.m
@@ -14,21 +14,27 @@ dataset = xlsread('BD_COUNTRY_RISK_EU.ods','BDTOTAL');
 % Prepare data
 [Xtrain,Ytrain1,Ytrain2,Ytrain3,Ntrain,Xtest,Ytest1,Ytest2,Ytest3,Ntest,N,J,K] = initData(dataset);
 
-% Get optimal value of k neighbours
+% Get optimal value of k neighbours agency 1
 k1 = findOptimalNeighbours(Xtrain,Ytrain1,Ntrain);
-k2 = findOptimalNeighbours(Xtrain,Ytrain2,Ntrain);
-k3 = findOptimalNeighbours(Xtrain,Ytrain3,Ntrain);
-
-% Apply knn algorithm 
+% Apply knn algorithm agency 1
 [Ypredicted1,CCR1] = knnAlgorithm(Xtrain,Xtest,k1,Ytrain1,Ytest1,Ntest);
-[Ypredicted2,CCR2] = knnAlgorithm(Xtrain,Xtest,k2,Ytrain2,Ytest2,Ntest);
-[Ypredicted3,CCR3] = knnAlgorithm(Xtrain,Xtest,k3,Ytrain3,Ytest3,Ntest);
-
-% Show results
+% Show results agency 1
 disp("RATE AGENCY 1) S&P: ")
 showResult(k1,CCR1);
+
+% Get optimal value of k neighbours agency 2
+k2 = findOptimalNeighbours(Xtrain,Ytrain2,Ntrain);
+% Apply knn algorithm agency 2
+[Ypredicted2,CCR2] = knnAlgorithm(Xtrain,Xtest,k2,Ytrain2,Ytest2,Ntest);
+% Show results agency 2
 disp("RATE AGENCY 2) Moodys: ")
 showResult(k2,CCR2);
+
+% Get optimal value of k neighbours agency 3
+k3 = findOptimalNeighbours(Xtrain,Ytrain3,Ntrain);
+% Apply knn algorithm agency 3
+[Ypredicted3,CCR3] = knnAlgorithm(Xtrain,Xtest,k3,Ytrain3,Ytest3,Ntest);
+% Show results agency 3
 disp("RATE AGENCY 3) Fitch: ")
 showResult(k3,CCR3);
 
