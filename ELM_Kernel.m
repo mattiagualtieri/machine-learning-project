ef9a611e5264955a40abf8266a7deb765fdcc33d
Carlos Cuevas <ccuevasbalinas@gmail.com>
2021-11-28 09:58:38 +0100

    ELM Kernel Final

--- ELM_Kernel.m
+++ ELM_Kernel.m
@@ -1,8 +1,8 @@
-% ===========================================================  %
+% ======================================================================= %
 
-% Kernel Extreme Learning Machine Algorithm 
-% Carlos Cuevas Baliñas
-% Machine Learning - 4º IITV 
+% MACHINE LEARNING PROJECT - MACHINE LEARNING 4ºIITV
+% Group 1: Addressing the EU Sovereign Ratings
+% Álvaro Bersabé, Carlos Cuevas, Mattia Gualtieri, Álvaro Jiménez
 
 % Initial configuration 
 clear all;
@@ -23,32 +23,40 @@ Ytest1 = generate1ofJLabel(Ytest1,Ntest,J);
 Ytest2 = generate1ofJLabel(Ytest2,Ntest,J);
 Ytest3 = generate1ofJLabel(Ytest3,Ntest,J);
 
-% Get optimal value of C and sigma agency 1
-[C_optimal1,sigma_optimal1] = findOptimalHyperparameters(Xtrain,Ytrain1,Ntrain);
-% Apply Kernel Extreme Learning Machine agency 1
-[Ypredicted1,CCR1,CCR_ELM1] = kernelExtremeLearningMachine(Xtrain,Ytrain1,Xtest,Ytest1,Ntrain,Ntest,C_optimal1,sigma_optimal1);
-% Show results agency 1
-disp("RATE AGENCY 1) S&P: ")
-showResult(C_optimal1,sigma_optimal1,CCR1,CCR_ELM1);
+% Select Rating Agency 
+agency = 3;  % 1 = S&P //  2 = Moodys // 3 = Fitch
 
-% Get optimal value of C and sigma agency 2
-[C_optimal2,sigma_optimal2] = findOptimalHyperparameters(Xtrain,Ytrain2,Ntrain);
-% Apply Kernel Extreme Learning Machine agency 2
-[Ypredicted2,CCR2,CCR_ELM2] = kernelExtremeLearningMachine(Xtrain,Ytrain2,Xtest,Ytest2,Ntrain,Ntest,C_optimal2,sigma_optimal2);
-% Show results agency 2
-disp("RATE AGENCY 2) Moodys: ")
-showResult(C_optimal2,sigma_optimal2,CCR2,CCR_ELM2);
+% ELM Kernel for agency 1
+if agency == 1
+    [Ypredicted,CCR,CCR_ELM,C,sigma] = elmKernel(Xtrain,Xtest,Ytrain1,Ytest1,Ntrain,Ntest);
+    disp("=======================================================================");
+    disp("RATE AGENCY 1) S&P: ")
+    showResult(C,sigma,CCR,CCR_ELM);
+    disp("=======================================================================");
+end
+% ELM Kernel for agency 2
+if agency == 2
+    [Ypredicted,CCR,CCR_ELM,C,sigma] = elmKernel(Xtrain,Xtest,Ytrain2,Ytest2,Ntrain,Ntest);
+    disp("=======================================================================");
+    disp("RATE AGENCY 2) Moodys: ")
+    showResult(C,sigma,CCR,CCR_ELM);
+    disp("=======================================================================");
+end
+% ELM Kernel for agency 3
+if agency == 3
+    [Ypredicted,CCR,CCR_ELM,C,sigma] = elmKernel(Xtrain,Xtest,Ytrain3,Ytest3,Ntrain,Ntest);
+    disp("=======================================================================");
+    disp("RATE AGENCY 3) Fitch: ")
+    showResult(C,sigma,CCR,CCR_ELM);
+    disp("=======================================================================");
+end
+% Error
+if agency < 1 || agency > 3
+    disp("Error selecting agency");
+end
+% ======================================================================= %
 
-% Get optimal value of C and sigma agency 3
-[C_optimal3,sigma_optimal3] = findOptimalHyperparameters(Xtrain,Ytrain3,Ntrain);
-% Apply Kernel Extreme Learning Machine agency 3
-[Ypredicted3,CCR3,CCR_ELM3] = kernelExtremeLearningMachine(Xtrain,Ytrain3,Xtest,Ytest3,Ntrain,Ntest,C_optimal3,sigma_optimal3);
-% Show results agency 3
-disp("RATE AGENCY 3) Fitch: ")
-showResult(C_optimal3,sigma_optimal3,CCR3,CCR_ELM3);
-% ===========================================================  %
-
-% ===========================================================  %
+% ======================================================================= %
 function Y = generate1ofJLabel(originalY,Ntrain,J)
     
     % Generate class label according to 1 of J
@@ -59,9 +67,18 @@ function Y = generate1ofJLabel(originalY,Ntrain,J)
     end
     
 end
-% ===========================================================  %
+% ======================================================================= %
 
-% ===========================================================  %
+% ======================================================================= %
+function [Ypredicted,CCR,CCR_ELM,C,sigma] = elmKernel(Xtrain,Xtest,Ytrain,Ytest,Ntrain,Ntest)
+    % Get optimal value of C and sigma
+    [C,sigma] = findOptimalHyperparameters(Xtrain,Ytrain,Ntrain);
+    % Apply Kernel Extreme Learning Machine
+    [Ypredicted,CCR,CCR_ELM] = kernelExtremeLearningMachine(Xtrain,Ytrain,Xtest,Ytest,Ntrain,Ntest,C,sigma);
+end
+% ======================================================================= %
+
+% ======================================================================= %
 function [C_optimal,sigma_optimal] = findOptimalHyperparameters(Xtrain,Ytrain,Ntrain)
 
     % Split training data into trainVal and testVal
@@ -104,9 +121,9 @@ function [C_optimal,sigma_optimal] = findOptimalHyperparameters(Xtrain,Ytrain,Nt
     sigma_optimal = arrayCost(indexMin,3);
 
 end
-% ===========================================================  %
+% ======================================================================= %
 
-% ===========================================================  %
+% ======================================================================= %
 function [Ypredicted,CCR,CCR_ELM] = kernelExtremeLearningMachine(Xtrain,Ytrain,Xtest,Ytest,Ntrain,Ntest,C,sigma)
     
     % Generate omega
@@ -122,12 +139,13 @@ function [Ypredicted,CCR,CCR_ELM] = kernelExtremeLearningMachine(Xtrain,Ytrain,X
         [~, position] = max(Ypredicted(i,:));
         predicts(i, position) = 1;
     end
+    
     CCR = sum(predicts == Ytest)/Ntest;
     CCR_ELM = mean(CCR);
 end
-% ===========================================================  %
+% ======================================================================= %
 
-% ===========================================================  %
+% ======================================================================= %
 function omega = generateOmega(X,N,sigma)
     % Calcultate omega -> omega = H'*H
     omega = zeros(N,N);
@@ -138,9 +156,9 @@ function omega = generateOmega(X,N,sigma)
         end
     end
 end
-% ===========================================================  %
+% ======================================================================= %
 
-% ===========================================================  %
+% ======================================================================= %
 function KM = generateKernelMatrix(X1,X2,N1,N2,sigma)
     KM = zeros(N2,N1);
     for i=1:1:N2
@@ -150,9 +168,9 @@ function KM = generateKernelMatrix(X1,X2,N1,N2,sigma)
         end
     end
 end
-% ===========================================================  %
+% ======================================================================= %
 
-% ===========================================================  %
+% ======================================================================= %
 function GK = gaussianKernel(x,y,sigma)
     % Radial Basis Function 
     aux1 = norm(x-y);
@@ -160,9 +178,9 @@ function GK = gaussianKernel(x,y,sigma)
     aux3 = -(aux1/aux2);
     GK = exp(aux3);
 end
-% ===========================================================  %
+% ======================================================================= %
 
-% ===========================================================  %
+% ======================================================================= %
 function showResult(C,sigma,CCR,CCR_ELM)
     String1 = ['Optimal C hyperparameter: ',num2str(C)];
     String2 = ['Optimal sigma hyperparameter: ',num2str(sigma)];
@@ -172,9 +190,8 @@ function showResult(C,sigma,CCR,CCR_ELM)
     disp(CCR);
     String3 = ['CCR Kernel ELM Algorithm: ',num2str(CCR_ELM)];
     disp(String3);
-    disp(" ")
 end
-% ===========================================================  %
+% ======================================================================= %
 
 
 
